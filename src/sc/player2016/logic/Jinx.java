b6a79f14c80b100b8d46e95a727fb6540e896e55
gordonkoehn <gordon@koehn.net>
2015-12-29 22:01:37 +0100

    Dynamic visual Range

--- src/sc/player2016/logic/Jinx.java
+++ src/sc/player2016/logic/Jinx.java
@@ -10,76 +10,109 @@ import sc.plugin2016.Move;
 import sc.player2016.logic.Field;
 import sc.player2016.logic.Board;
 
-/**
- *
- * @author gordonkoehn
- */
+
 public class Jinx {
 
     static int x = 6;
     static int y = 6;
     
-    static int visualRange = 2; /*not in use**/
+    public static int visualRange = 2; // 2 or 1
     
-    static Move lastlastMove;  /*not in use**/
+    static Move lastlastMove;  
+    
+    public static boolean firstMove = true;
     
     public static Board board ;  /*not in use**/
      
-
+    
     public static Move findMove (GameState gameState){
         
         Move selection;
         
-        preselectMoves(gameState);
+        if (gameState.getTurn() == 3) {firstMove = false;};
+        
+        preselectMoves(gameState, lastlastMove);
+        
+        //Just for Testing 
+        if (gameState.getTurn() == 3) {x = 18;y = 7;}
+        if (gameState.getTurn() == 5) {x = 18;y = 9;}
         
         selection = new Move(x, y); 
-        
-        
-
+ 
         return selection;
     } 
 
+    public static ArrayList<Field> preselectMoves (GameState gameState, Move lastlastMove){
+         ArrayList<Field> preselectMoves = new ArrayList();
+         
+         
+           /* preselected moves in realtion to a move **/ 
+         int[][] visualRange1 = {
+                      {-1,2},{-1,-2},{1,2},{-2,1},{2,1},{-2,-1},{2,-1},{1,-2} 
+                };    
+           
+         int[][] visualRange2 = {
+                          {-2,4},{0,4},{2,4},
+                       {-3,3},{-1,3},{1,3},{3,3},
+                     {-4,2},                      {-4,2},
+                          {-3,1},             {3,1},
+                     {-4,0},                         {4,0},     
+                          {-3,-1},             {3,-1},
+                     {-4,-2},                      {-4,-2},
+                       {-3,-3},{-1,-3},{1,-3},{3,-3},        
+                         {-2,-4},{0,-4},{2,-4}
+         };   
+         
+         int[][] visualRangeSel = null;       
+                 
+         switch (visualRange) {
+            case 1:  visualRange = 1;
+                     visualRangeSel = new int[visualRange1.length][visualRange1.length];
+                     System.arraycopy(visualRange1, 0, visualRangeSel, 0, visualRange1.length);
+                     break;
+            case 2:  visualRange = 2;                   
+                     visualRangeSel = new int[visualRange1.length + visualRange2.length][visualRange1.length + visualRange2.length ];
+
+                     System.arraycopy(visualRange1, 0, visualRangeSel, 0, visualRange1.length);
+                     System.arraycopy(visualRange2, 0, visualRangeSel, visualRange1.length, visualRange2.length);
+                     
+                     break;
+            default:break;
+        }
+          
+         preselectMoves.addAll(pMsub(gameState.getLastMove(), gameState, visualRangeSel));
+         
+         if (gameState.getTurn() >= 4){ //if GrandKo starts second
+         preselectMoves.addAll(pMsub(lastlastMove, gameState, visualRangeSel));
+         }
+         
+         return preselectMoves;
+    }
     
-    public static ArrayList<Field> preselectMoves (GameState gameState){
-        ArrayList<Field> preselectMoves = new ArrayList();
-  
-        /* preselected moves in realtion to a move **/ 
-        int[][] visualRange1 = {
-                            {-1,2},{-1,-2},{1,2},{-2,1},{2,1},{-2,-1},{2,-1},{1,-2} 
-        };    
-        //Not IN USE
-        int[][] visualRange2 = {
-                    {-2,4},{0,4},{2,4},
-                 {-3,3},{-1,3},{1,3},{3,3},
-               {-4,2},                      {-4,2},
-                    {-3,1},             {3,1},
-               {-4,0},                         {4,0},     
-                    {-3,-1},             {3,-1},
-               {-4,-2},                      {-4,-2},
-                 {-3,-3},{-1,-3},{1,-3},{3,-3},        
-                   {-2,-4},{0,-4},{2,-4}
-        };    
+    private static ArrayList<Field> pMsub(Move occuMove, GameState gameState, int[][] visualRangeSel) {
         
-        Move lastMove = gameState.getLastMove();
-        
-        for (int[] a : visualRange1){
-            int Mx = lastMove.getX();
-            int My = lastMove.getY(); 
-            
-            Mx = Mx + a[0];        
-            My = My + a[1];
-            
-            Field relatedMove = new Field(Mx, My);
+                ArrayList<Field> preselectMoves = new ArrayList();
                 
-           relatedMove.setType(Field.Type.Free);
-            
-            if (Mx >= 0 && Mx < 24 && My >= 0 && My < 24){
-                if (Field.Type.Blocked != relatedMove.getType()){
-                    preselectMoves.add(relatedMove);
-                    System.out.println(relatedMove.x + ", " + relatedMove.y);
+                for (int[] a : visualRangeSel){
+                int Mx = occuMove.getX();
+                int My = occuMove.getY(); 
+
+                Mx = Mx + a[0];        
+                My = My + a[1];
+
+                Field relatedMove = new Field(Mx, My);
+
+               relatedMove.setType(Field.Type.Free); // Just for testing
+
+                if (Mx >= 0 && Mx < 24 && My >= 0 && My < 24){
+                    if (Field.Type.Blocked != relatedMove.getType()){
+                        preselectMoves.add(relatedMove);
+                        System.out.println(relatedMove.x + ", " + relatedMove.y);
+                    }
                 }
             }
-        }
-        return preselectMoves;
-    }
+            lastlastMove = gameState.getLastMove();
+            return preselectMoves;   
+            }
 }
+
